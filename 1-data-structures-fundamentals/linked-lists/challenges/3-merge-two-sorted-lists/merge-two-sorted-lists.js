8a46ade30eb91d6fe47bd4a8798f2744fda03559
Jonathan Veloso <velosojonathan5@gmail.com>
2026-03-13 16:38:53 -0300

    improve merge two sorted linked lists

--- 1-data-structures-fundamentals/linked-lists/challenges/3-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 1-data-structures-fundamentals/linked-lists/challenges/3-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -12,28 +12,14 @@
 
 
 function mergeTwoLists(list1, list2) {
-    let currentNode = new ListNode(0);
-    let head = currentNode;
+    if(list1 == null) return list2;
+    if(list2 == null) return list1;
 
-    while (list1 != null && list2 != null) {
-
-        if(list1.val < list2.val) {
-            currentNode.next = new ListNode(list1.val);
-            currentNode = currentNode.next;
-            list1 = list1.next;
-        } else {
-            currentNode.next = new ListNode(list2.val);
-            currentNode = currentNode.next;
-            list2 = list2.next;
-        }
+    if(list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-
-    if (list1 != null) {
-        currentNode.next = list1;
-    }
-    if (list2 != null) {
-        currentNode.next = list2;
-    }
-
-    return head.next;
 };
